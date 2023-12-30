eb4ef1902bd411c6c40cc0c3856d437d9e79e23e
muhammad umair <muhammadumairabbasi637@gmail.com>
2023-12-30 17:31:39 +0500

    created Cards with slider

--- task-one/src/App.jsx
+++ task-one/src/App.jsx
@@ -7,14 +7,14 @@ function App() {
     <>
       <div className="bg-[#21313C] pt-8">
         <div className="text-center">
-          <h1 className="text-white text-4xl px-2">
+          <h1 className="text-white text-4xl  px-1">
             Angle Brackets Mobile App{" "}
           </h1>
           <h1 className="text-3xl  text-[#ede4e4a9] font-bold pt-2  ">
             {" "}
             development experts
           </h1>
-          <p className="text-[#95afbf] text-sm pt-2 px-0.5">
+          <p className="text-[#95afbf] text-sm pt-2 px-2">
             Angle Brackets will quickly assemble a team of Front-end developers
             for your needs
           </p>
